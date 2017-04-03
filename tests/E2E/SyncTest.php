contentful/contentful.php

commit 2bc920c2c25a3c8773a36d5d57a710486ced3d9b

Adds support for sync in the Preview API.

src/Delivery/Synchronization/Manager.php
@@ -30,20 +30,27 @@ class Manager
     private $builder;
 
     /**
+     * @var bool
+     */
+    private $preview;
+
+    /**
      * Manager constructor.
      *
      * Do not instantiate this class yourself, use Contentful\Delivery\Client::getSynchronizationManager instead.
      *
      * @param Client          $client
      * @param ResourceBuilder $builder
+     * @param bool            $preview
      *
      * @see \Contentful\Delivery\Client::get
      * @internal
      */
-    public function __construct(Client $client, ResourceBuilder $builder)
+    public function __construct(Client $client, ResourceBuilder $builder, $preview)
     {
         $this->client = $client;
         $this->builder = $builder;
+        $this->preview = $preview;
     }
 
     /**
@@ -51,7 +58,7 @@ class Manager
      *
      * By calling Result::isDone it can be checked if there's another page of results, if so call `continueSync` to get the next page.
      *
-     * A Query can be used to reduce only a subset of the space.
+     * A Query can be used to return only a subset of the space.
      *
      * @param  Query|null $query
      *
@@ -74,11 +81,16 @@ class Manager
      *
      * @return Result
      *
+     * @throws \RuntimeException If this method is used for a subsequent sync when used with the Preview API.
+     *
      * @api
      */
     public function continueSync($token)
     {
         if ($token instanceof Result) {
+            if ($this->preview && $token->isDone()) {
+                throw new \RuntimeException();
+            }
             $token = $token->getToken();
         }
 
@@ -103,7 +115,12 @@ class Manager
             $done = false;
             $token = $this->getTokenFromUrl($data['nextPageUrl']);
         }
-        $items = array_map([$this->builder, 'buildObjectsFromRawData'], $data['items']);
+        $items = array_map(function ($item) {
+            if (isset($item['sys']['locale'])) {
+                unset($item['sys']['locale']);
+            }
+            $this->builder->buildObjectsFromRawData($item);
+        }, $data['items']);
 
         return new Result($items, $token, $done);
     }

tests/E2E/SyncTest.php
@@ -12,31 +12,36 @@ use Contentful\Delivery\Synchronization\Result;
 class SyncTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var Client
-     */
-    private $client;
-
-    public function setUp()
-    {
-        $this->client = new Client('b4c0n73n7fu1', 'cfexampleapi');
-    }
-
-    /**
      * @vcr e2e_sync_basic.json
      */
     public function testBasicSync()
     {
-        $mananger = $this->client->getSynchronizationManager();
+        $manager = (new Client('b4c0n73n7fu1', 'cfexampleapi'))
+            ->getSynchronizationManager();
 
-        $result = $mananger->startSync();
+        $result = $manager->startSync();
 
         $this->assertInstanceOf(Result::class, $result);
         $this->assertEquals('w5ZGw6JFwqZmVcKsE8Kow4grw45QdybCnV_Cg8OASMKpwo1UY8K8bsKFwqJrw7DDhcKnM2RDOVbDt1E-wo7CnDjChMKKGsK1wrzCrBzCqMOpZAwOOcOvCcOAwqHDv0XCiMKaOcOxZA8BJUzDr8K-wo1lNx7DnHE', $result->getToken());
         $this->assertTrue($result->isDone());
 
-        $result2 = $mananger->continueSync($result);
+        $result2 = $manager->continueSync($result);
 
         $this->assertInstanceOf(Result::class, $result2);
         $this->assertTrue($result2->isDone());
+    }
+
+    /**
+     * @vcr e2e_sync_preview.json
+     */
+    public function testPreviewSync()
+    {
+        $manager = (new Client('e5e8d4c5c122cf28fc1af3ff77d28bef78a3952957f15067bbc29f2f0dde0b50', 'cfexampleapi', true))
+            ->getSynchronizationManager();
+
+        $result = $manager->startSync();
+
+        $this->assertInstanceOf(Result::class, $result);
+        $this->assertTrue($result->isDone());
     }
 }

src/Delivery/Client.php
@@ -291,22 +291,17 @@ class Client extends BaseClient
     }
 
     /**
-     * Get an instance of the synchronization manager.
+     * Get an instance of the synchronization manager. Note that with the Preview API only an inital sync
+     * is giving valid results.
      *
      * @return Manager
-     *
-     * @throws \RuntimeException If this method is called while using the Preview API.
      *
      * @see https://www.contentful.com/developers/docs/concepts/sync/ Sync API
      * @api
      */
     public function getSynchronizationManager()
     {
-        if ($this->preview) {
-            throw new \RuntimeException('SynchronizationManager is not available for the Preview API.');
-        }
-
-        return new Manager($this, $this->builder);
+        return new Manager($this, $this->builder, $this->preview);
     }
 
     private function requestAndBuild($method, $path, array $options = [])

tests/Unit/Delivery/ClientTest.php
@@ -31,14 +31,4 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 
         $this->assertTrue($client->isPreview());
     }
-
-    /**
-     * @expectedException \RuntimeException
-     */
-    public function testGetSynchronizationPreview()
-    {
-        $client = new Client('b4c0n73n7fu1', 'cfexampleapi', true);
-
-        $client->getSynchronizationManager();
-    }
 }

commit 25aacc706545e72b0d8023ff9112e84935c643d1

Fixes sync returning an array of NULLs instead of objects.
Regression introduced in 2bc920c2c25a3c8773a36d5d57a710486ced3d9b (#64)

src/Delivery/Synchronization/Manager.php
@@ -119,7 +119,7 @@ class Manager
             if (isset($item['sys']['locale'])) {
                 unset($item['sys']['locale']);
             }
-            $this->builder->buildObjectsFromRawData($item);
+            return $this->builder->buildObjectsFromRawData($item);
         }, $data['items']);
 
         return new Result($items, $token, $done);

tests/E2E/SyncTest.php
@@ -7,7 +7,9 @@
 namespace Contentful\Tests\E2E;
 
 use Contentful\Delivery\Client;
+use Contentful\Delivery\DynamicEntry;
 use Contentful\Delivery\Synchronization\Result;
+use Contentful\Tests\Unit\Delivery\DynamicEntryTest;
 
 class SyncTest extends \PHPUnit_Framework_TestCase
 {
@@ -24,6 +26,9 @@ class SyncTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(Result::class, $result);
         $this->assertEquals('w5ZGw6JFwqZmVcKsE8Kow4grw45QdybCnV_Cg8OASMKpwo1UY8K8bsKFwqJrw7DDhcKnM2RDOVbDt1E-wo7CnDjChMKKGsK1wrzCrBzCqMOpZAwOOcOvCcOAwqHDv0XCiMKaOcOxZA8BJUzDr8K-wo1lNx7DnHE', $result->getToken());
         $this->assertTrue($result->isDone());
+
+        $items = $result->getItems();
+        $this->assertInstanceOf(DynamicEntry::class, $items[0]);
 
         $result2 = $manager->continueSync($result);
 
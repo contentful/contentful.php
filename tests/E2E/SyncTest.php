25aacc706545e72b0d8023ff9112e84935c643d1
Rouven Weßling <rouven@contentful.com>
2017-04-03 19:48:16 +0200

    Fixes sync returning an array of NULLs instead of objects.
    Regression introduced in 2bc920c2c25a3c8773a36d5d57a710486ced3d9b (#64)

--- tests/E2E/SyncTest.php
+++ tests/E2E/SyncTest.php
@@ -7,7 +7,9 @@
 namespace Contentful\Tests\E2E;
 
 use Contentful\Delivery\Client;
+use Contentful\Delivery\DynamicEntry;
 use Contentful\Delivery\Synchronization\Result;
+use Contentful\Tests\Unit\Delivery\DynamicEntryTest;
 
 class SyncTest extends \PHPUnit_Framework_TestCase
 {
@@ -24,6 +26,9 @@ class SyncTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(Result::class, $result);
         $this->assertEquals('w5ZGw6JFwqZmVcKsE8Kow4grw45QdybCnV_Cg8OASMKpwo1UY8K8bsKFwqJrw7DDhcKnM2RDOVbDt1E-wo7CnDjChMKKGsK1wrzCrBzCqMOpZAwOOcOvCcOAwqHDv0XCiMKaOcOxZA8BJUzDr8K-wo1lNx7DnHE', $result->getToken());
         $this->assertTrue($result->isDone());
+
+        $items = $result->getItems();
+        $this->assertInstanceOf(DynamicEntry::class, $items[0]);
 
         $result2 = $manager->continueSync($result);
 
