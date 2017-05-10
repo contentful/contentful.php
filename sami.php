6391c0348af0d32561c8cc293268aa4681a4faa6
Rouven Weßling <rouven@contentful.com>
2017-05-10 17:38:13 +0200

    Adds automatic publishing of the reference docs to GitHub pages.
    A new deployment is triggered everytime a new tag is created.

--- sami.php
+++ sami.php
@@ -8,16 +8,25 @@
 // https://github.com/FriendsOfPHP/Sami
 
 use Sami\Sami;
+use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
+
+$dir = __DIR__ . '/src';
 
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->in(__DIR__ . '/src')
+    ->in($dir)
+;
+
+// generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('*')
 ;
 
 return new Sami($iterator, [
     'title'     => 'Contentful CDA SDK for PHP',
-    'build_dir' => __DIR__ . '/build/docs',
-    'cache_dir' => __DIR__ . '/build/cache',
+    'versions'  => $versions,
+    'build_dir' => __DIR__ . '/build/docs/%version%',
+    'cache_dir' => __DIR__ . '/build/doc_cache/%version%',
 ]);
