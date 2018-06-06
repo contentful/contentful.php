7f520af03aed8a5c4b8914d09473662b674eb995
Davide Borsatto <davide.borsatto@gmail.com>
2018-06-06 14:13:26 +0200

    chore(cs): Update php-cs-fixer config

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the contentful.php package.
+ * This file is part of the contentful/contentful package.
  *
  * @copyright 2015-2018 Contentful GmbH
  * @license   MIT
