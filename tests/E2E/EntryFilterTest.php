b9d115f9f35202f43eeea6a989f74c2e22053646
Davide Borsatto <davide.borsatto@gmail.com>
2018-08-09 16:32:23 +0200

    chore(coverage): Add filtering tests

---
+++ tests/E2E/EntryFilterTest.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * This file is part of the contentful/contentful package.
+ *
+ * @copyright 2015-2018 Contentful GmbH
+ * @license   MIT
+ */
+
+namespace Contentful\Tests\Delivery\E2E;
+
+use Contentful\Delivery\Query;
+use Contentful\Delivery\Resource\Entry;
+use Contentful\Tests\Delivery\TestCase;
+
+class EntryFilterTest extends TestCase
+{
+    /**
+     * @vcr e2e_entry_filter_all.json
+     */
+    public function testAll()
+    {
+        $client = $this->getClient('cfexampleapi');
+
+        // entries?content_type={content_type}&{attribute}%5Bnin%5D={value}
+        $query = (new Query())
+            ->setContentType('cat')
+            ->where('sys.id', ['nyancat'], 'nin')
+        ;
+        $entries = $client->getEntries($query);
+        /** @var Entry $entry */
+        foreach ($entries as $entry) {
+            if ('nyancat' === $entry->getId()) {
+                $this->fail('Entry with ID "nyancat" was fetched even though it should not have.');
+            }
+        }
+
+        // entries?content_type={content_type}&{attribute}%5Bexists%5D={value}
+        $query = (new Query())
+            ->setContentType('cat')
+            ->where('fields.name', 'true', 'exists')
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertCount(3, $entries);
+
+        // entries?{attribute}%5Blte%5D={value}
+        $query = (new Query())
+            ->where('sys.createdAt', '2013-06-27T22:46:19.513Z', 'lte')
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertCount(1, $entries);
+
+        // entries?content_type={content_type}&fields.{field_id}%5Bmatch%5D={value}
+        $query = (new Query())
+            ->setContentType('cat')
+            ->where('fields.name', 'cat', 'match')
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertCount(2, $entries);
+
+        // entries?fields.center%5Bnear%5D={coordinate}&content_type={content_type}
+        $query = (new Query())
+            ->setContentType('1t9IbcfdCk6m04uISSsaIK')
+            ->where('fields.center', '51.508530,-0.076132', 'near')
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertSame('London', $entries[0]->get('name'));
+
+        // entries?fields.center%5Bwithin%5D={rectangle}&content_type={content_type}
+        $query = (new Query())
+            ->setContentType('1t9IbcfdCk6m04uISSsaIK')
+            ->where('fields.center', '48,-1,52,0', 'within')
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertSame('London', $entries[0]->get('name'));
+
+        // entries?order={attribute}
+        $query = (new Query())
+            ->orderBy('sys.createdAt')
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertSame('nyancat', $entries[0]->getId());
+
+        // entries?order=-{attribute}
+        $query = (new Query())
+            ->orderBy('sys.createdAt', true)
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertSame('San Francisco', $entries[0]->get('name'));
+
+        // entries?order={attribute},{attribute2}
+        $query = (new Query())
+            ->orderBy('sys.createdAt,sys.revision')
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertSame('nyancat', $entries[0]->getId());
+
+        // entries?limit={value}
+        $query = (new Query())
+            ->setLimit(1)
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertCount(1, $entries);
+
+        // entries?skip={value}
+        $query = (new Query())
+            ->setSkip(1)
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertInstanceOf(Entry::class, $entries[0]);
+
+        // entries?content_type={content_type}&fields.{linking_field}.sys.id={target_entry_id}
+        $query = (new Query())
+            ->setContentType('cat')
+            ->where('fields.bestFriend.sys.id', 'happycat')
+        ;
+        $entries = $client->getEntries($query);
+        $this->assertCount(1, $entries);
+        $this->assertSame('nyancat', $entries[0]->getId());
+
+        // assets?mimetype_group={mimetype_group}
+        $query = (new Query())
+            ->setMimeTypeGroup('image')
+        ;
+        $assets = $client->getAssets($query);
+        $this->assertCount(4, $assets);
+    }
+}
