95cd46e435a548b7e6ee7080fcfecdc869708edf
Davide Borsatto <davide.borsatto@gmail.com>
2018-08-23 12:15:11 +0200

    feat(json-decoder): Move JSON parsing to a separate class

--- src/Client.php
+++ src/Client.php
@@ -93,6 +93,11 @@ class Client extends BaseClient
     private $environmentId;
 
     /**
+     * @var ScopedJsonDecoder
+     */
+    private $scopedJsonDecoder;
+
+    /**
      * Client constructor.
      *
      * @param string      $token         Delivery API Access Token for the space used with this Client
@@ -148,6 +153,7 @@ class Client extends BaseClient
             $this->environmentId
         );
         $this->builder = new ResourceBuilder($this, $this->instanceRepository);
+        $this->scopedJsonDecoder = new ScopedJsonDecoder($this->spaceId, $this->environmentId);
     }
 
     /**
@@ -421,95 +427,9 @@ class Client extends BaseClient
      */
     public function parseJson($json)
     {
-        $data = \GuzzleHttp\json_decode($json, true);
-
-        $spaceId = $this->extractSpaceId($data);
-        $environmentId = $this->extractEnvironmentId($data);
-        if ($spaceId !== $this->spaceId || $environmentId !== $this->environmentId) {
-            throw new \InvalidArgumentException(\sprintf(
-                'Trying to parse and build a JSON structure with a client configured for handling space "%s" and environment "%s", but space "%s" and environment "%s" were detected.',
-                $this->spaceId,
-                $this->environmentId,
-                $spaceId,
-                $environmentId
-            ));
-        }
-
-        return $this->builder->build($data);
-    }
-
-    /**
-     * Checks a data structure and extracts the space ID, if present.
-     *
-     * @param array $data
-     *
-     * @return string|null
-     */
-    private function extractSpaceId(array $data)
-    {
-        // Space resource
-        if (isset($data['sys']['type']) && 'Space' === $data['sys']['type']) {
-            return $data['sys']['id'];
-        }
-
-        // Environment resource
-        if (isset($data['sys']['type']) && 'Environment' === $data['sys']['type']) {
-            return $this->spaceId;
-        }
-
-        // Resource linked to a space
-        if (isset($data['sys']['space'])) {
-            return $data['sys']['space']['sys']['id'];
-        }
-
-        // Array resource with at least an element
-        if (isset($data['items'][0]['sys']['space'])) {
-            return $data['items'][0]['sys']['space']['sys']['id'];
-        }
-
-        // Empty array resource
-        if (isset($data['items']) && !$data['items']) {
-            return $this->spaceId;
-        }
-
-        return '[blank]';
-    }
-
-    /**
-     * Checks a data structure and extracts the environment ID, if present.
-     *
-     * @param array $data
-     *
-     * @return string|null
-     */
-    public function extractEnvironmentId(array $data)
-    {
-        // Space resource
-        if (isset($data['sys']['type']) && 'Space' === $data['sys']['type']) {
-            return $this->environmentId;
-        }
-
-        // Environment resource
-        if (isset($data['sys']['type']) && 'Environment' === $data['sys']['type']) {
-            return $data['sys']['id'];
-        }
-
-        // Resource linked to a environment
-        if (isset($data['sys']['environment'])) {
-            return $data['sys']['environment']['sys']['id'];
-        }
-
-        // Array resource with at least an element
-        if (isset($data['items'][0]['sys']['environment'])) {
-            return $data['items'][0]['sys']['environment']['sys']['id'];
-        }
-
-        // Empty array resource
-        if (isset($data['items']) && !$data['items']) {
-            return $this->environmentId;
-        }
-
-        return 'master';
+        return $this->builder->build(
+            $this->scopedJsonDecoder->decode($json)
+        );
     }
 
     /**
---
+++ tests/Unit/ScopedJsonDecoderTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * This file is part of the contentful/contentful package.
+ *
+ * @copyright 2015-2018 Contentful GmbH
+ * @license   MIT
+ */
+
+namespace Contentful\Tests\Delivery\Unit;
+
+use Contentful\Delivery\ScopedJsonDecoder;
+use Contentful\Tests\Delivery\TestCase;
+
+class ScopedJsonDecoderTest extends TestCase
+{
+    /**
+     * @var ScopedJsonDecoder
+     */
+    private $scopedJsonDecoder;
+
+    public function setUp()
+    {
+        $this->scopedJsonDecoder = new ScopedJsonDecoder('cfexampleapi', 'master');
+
+        parent::setUp();
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testParseJsonInvalid()
+    {
+        $this->scopedJsonDecoder->decode('{"sys": {"type": "}}');
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage Trying to parse and build a JSON structure with a client configured for handling space "cfexampleapi" and environment "master", but space "wrongspace" and environment "master" were detected.
+     */
+    public function testParseJsonSpaceMismatch()
+    {
+        $this->scopedJsonDecoder->decode($this->getFixtureContent('space_mismatch.json'));
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage Trying to parse and build a JSON structure with a client configured for handling space "cfexampleapi" and environment "master", but space "wrongspace" and environment "master" were detected.
+     */
+    public function testParseJsonContentTypeSpaceMismatch()
+    {
+        $this->scopedJsonDecoder->decode($this->getFixtureContent('content_type_space_mismatch.json'));
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage Trying to parse and build a JSON structure with a client configured for handling space "cfexampleapi" and environment "master", but space "[blank]" and environment "master" were detected.
+     */
+    public function testParseJsonEmptyObject()
+    {
+        $this->scopedJsonDecoder->decode('{}');
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage Trying to parse and build a JSON structure with a client configured for handling space "cfexampleapi" and environment "master", but space "invalidSpace" and environment "invalidEnvironment" were detected.
+     */
+    public function testParseJsonInvalidArray()
+    {
+        $this->scopedJsonDecoder->decode($this->getFixtureContent('invalid_array.json'));
+    }
+}
