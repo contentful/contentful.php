58fa1fcaa6f7f7d0e5ab8936a3e7e29e06cc0972
Paul Grigoruta <paul.grigoruta@gmail.com>
2020-02-24 16:07:19 +0200

    Fix test for self reference to check for an actual Entry when doing separate get calls

--- tests/E2E/EntryRichTextTest.php
+++ tests/E2E/EntryRichTextTest.php
@@ -14,7 +14,6 @@ namespace Contentful\Tests\Delivery\E2E;
 use Contentful\Delivery\Query;
 use Contentful\Delivery\Resource\Entry;
 use Contentful\RichText\Node\Document;
-use Contentful\RichText\Node\Nothing;
 use Contentful\RichText\Renderer;
 use Contentful\Tests\Delivery\TestCase;
 
@@ -61,14 +60,11 @@ class EntryRichTextTest extends TestCase
         $currentDepth = 1;
         while ($currentDepth < 100) {
             $entry = $entry->get('content')->getContent()[1];
-            if ($entry instanceof Nothing) {
-                break;
-            }
 
             $entry = $entry->getEntry();
             ++$currentDepth;
         }
 
-        $this->assertInstanceOf(Nothing::class, $entry);
+        $this->assertInstanceOf(Entry::class, $entry);
     }
 }
